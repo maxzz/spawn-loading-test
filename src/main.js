3c705844b9e0d6a5391ac4b303dd0ef716eea18e
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-28 18:31:18 -0700

    Add chalk errors highlight

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 const utils = require('util');
 const childProc = require('child_process');
+const chalk = require('chalk');
 
 const PORT = 4000;
 const MAX_CHILDREN = 5;
 
-main().catch(console.error);
+main().catch(function(error) {
+    console.error(chalk.red(error));
+});
 
 async function main() {
     throw 'EEE';
