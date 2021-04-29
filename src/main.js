df570a8d9eee2e6e0ae017907311ac7306585377
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-28 18:29:38 -0700

    Initial main test

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const utils = require('util');
+const childProc = require('child_process');
+
+const PORT = 4000;
+const MAX_CHILDREN = 5;
+
+main().catch(console.error);
+
+async function main() {
+    throw 'EEE';
+}
